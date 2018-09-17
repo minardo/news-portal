05df67292a969784d3e79a0645078f765145973e
minardo <ardo15.project@gmail.com>
2018-09-17 21:50:52 +0700

    Add GET Sources Interface

---
+++ app/src/main/java/com/betaministudio/application/newsportal/Interface/NewsService.java
@@ -0,0 +1,15 @@
+package com.betaministudio.application.newsportal.Interface;
+
+import com.betaministudio.application.newsportal.Model.WebSite;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ * Created by ARDO on 9/17/2018.
+ */
+
+public interface NewsService {
+    @GET("v1/sources?language=en")
+    Call<WebSite> getSources();
+}
